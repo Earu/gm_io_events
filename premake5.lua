60209733120e251e03cd369648055cff58e544f5
Earu <earu.derg@gmail.com>
2021-12-19 11:24:32 +0100

    Update projects for x64

--- premake5.lua
+++ premake5.lua
@@ -6,7 +6,7 @@ newoption({
 
 local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
-include(path.join(gmcommon, "generator.v2.lua"))
+include(path.join(gmcommon, "generator.v3.lua"))
 
 CreateWorkspace({name = "io_events"})
 	includedirs({"include"})
